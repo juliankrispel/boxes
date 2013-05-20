f560dc6d7b9781d9805cc1ec71b38c2f29cea3ec
Julian Krispel-Samsel <julian@goodafternoon.co>
2013-05-20 17:06:58 +0100

    Register the framework you nob

--- lib/boxes.rb
+++ lib/boxes.rb
@@ -1,8 +1,8 @@
 require 'boxes/version'
-#require 'compass'
-#root = File.join(File.dirname(__FILE__), "..")
-#
-#Compass::Frameworks.register("boxes",
-#    :stylesheets_directory => File.join(root,"sass"),
-#    :templates_directory => File.join(root,"templates")
-#)
+require 'compass'
+root = File.join(File.dirname(__FILE__), "..")
+
+Compass::Frameworks.register("boxes",
+    :stylesheets_directory => File.join(root,"sass"),
+    :templates_directory => File.join(root,"templates")
+)
