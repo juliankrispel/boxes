c595a54637d20f048f6583fc9a31372bd6f38c08
Julian Krispel-Samsel <juliankrispel@yahoo.de>
2013-05-21 11:23:42 +0100

    Fixed extension for use as ad-hoc compass plugin, removed deprecated compass module

--- lib/boxes.rb
+++ lib/boxes.rb
@@ -1,6 +1,8 @@
-require 'boxes/version'
 require 'compass'
 root = File.join(File.dirname(__FILE__), "..")
+
+$LOAD_PATH << File.dirname(__FILE__) #Fix $LOAD_PATH to load from this files directory to prevent error when loading this as ad-hoc extension
+require 'boxes/version.rb'
 
 Compass::Frameworks.register("boxes",
     :stylesheets_directory => File.join(root,"scss"),
